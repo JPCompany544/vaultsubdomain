688b3cd0aa8aee9f434a30233b503db39fdcf9dc
JPCompany544 <jaylaalexandria0@gmail.com>
2025-06-22 22:16:07 +0100

    final

--- src/App.js
+++ src/App.js
@@ -25,16 +25,17 @@ function App() {
 
       {/* Hero Section */}
       <section className="hero-container">
-        <div className="hero">
+       <div className="hero">
         {/* Token Icons Left - Hidden on Mobile */}
-        <div className="hero-left hidden md:block">
-          <div className="token-icons">
-            <img src="/images/ethereum-eth-logo.png" alt="ETH" className="token eth" />
-            <img src="/images/usd-coin-usdc-logo.png" alt="USDC" className="token usdc" />
-            <img src="/images/dai.png" alt="DAI" className="token dai" />
-            <img src="/images/litecoin.png" alt="WBTC" className="token wbtc" />
-          </div>
+      <div className="hidden md:flex relative w-[300px] h-[320px] items-center justify-center">
+        <div className="relative w-full h-full">
+          <img src="/images/ethereum-eth-logo.png" alt="ETH" className="token eth" />
+          <img src="/images/usd-coin-usdc-logo.png" alt="USDC" className="token usdc" />
+          <img src="/images/dai.png" alt="DAI" className="token dai" />
+          <img src="/images/litecoin.png" alt="WBTC" className="token wbtc" />
         </div>
+      </div>
+
 
           {/* Hero Center */}
           <div className="hero-content-wrapper">
